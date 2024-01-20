893d98ff9d3225d665ec29fb383ccd5c55ff1db8
Arthur Pereira da Silva <72280602+ArthurPdaSilva@users.noreply.github.com>
2024-01-20 10:12:53 -0300

    fix: corrigindo pause de música

--- src/domains/GameBox/features/GameBoxContainer/AudioPlayBackground/index.tsx
+++ src/domains/GameBox/features/GameBoxContainer/AudioPlayBackground/index.tsx
@@ -1,21 +1,17 @@
 import { CardMedia } from '@mui/material';
-import { useEffect, useState } from 'react';
 import trilha from '../../../../../assets/audios/trilha.mp3';
 import { useQuestionContext } from '../../../../../global/hooks/useQuestionContext';
 
 export default function AudioPlayBackground() {
   const { isFinish } = useQuestionContext();
-  const [isPlaying, setIsPlaying] = useState(true);
 
-  useEffect(() => {
-    if (isFinish()) {
-      setIsPlaying(false);
-    }
-  }, [isFinish]);
-
-  return (
-    <CardMedia component="audio" autoPlay={isPlaying}>
-      <source src={trilha} type="audio/mpeg" />
-    </CardMedia>
-  );
+  {
+    isFinish() ? (
+      <></>
+    ) : (
+      <CardMedia component="audio" autoPlay>
+        <source src={trilha} type="audio/mpeg" />
+      </CardMedia>
+    );
+  }
 }
